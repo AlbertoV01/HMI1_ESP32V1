bf00a9d11c73c535db99c3e84aec5a9217a9eea5
AlbertoV01 <albertovlzlar_@hotmail.com>
2025-01-20 16:02:35 -0700

    first commit

---
+++ HMI_ESP32V1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace HMI_ESP32V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_LedOn = new System.Windows.Forms.Button();
+            this.btn_LedOff = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Close);
+            this.groupBox1.Controls.Add(this.btn_Open);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 382);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM PORT SETTINGS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_LedOff);
+            this.groupBox2.Controls.Add(this.btn_LedOn);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(303, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(485, 411);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LED CONTROL";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORT:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(158, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(158, 197);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(9, 252);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(132, 23);
+            this.btn_Open.TabIndex = 0;
+            this.btn_Open.Text = "OPEN";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(147, 252);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(132, 23);
+            this.btn_Close.TabIndex = 4;
+            this.btn_Close.Text = "CLOSE";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(186, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 77);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_LedOn
+            // 
+            this.btn_LedOn.Location = new System.Drawing.Point(22, 163);
+            this.btn_LedOn.Name = "btn_LedOn";
+            this.btn_LedOn.Size = new System.Drawing.Size(75, 23);
+            this.btn_LedOn.TabIndex = 1;
+            this.btn_LedOn.Text = "LED ON";
+            this.btn_LedOn.UseVisualStyleBackColor = true;
+            this.btn_LedOn.Click += new System.EventHandler(this.btn_LedOn_Click);
+            // 
+            // btn_LedOff
+            // 
+            this.btn_LedOff.Location = new System.Drawing.Point(386, 163);
+            this.btn_LedOff.Name = "btn_LedOff";
+            this.btn_LedOff.Size = new System.Drawing.Size(75, 23);
+            this.btn_LedOff.TabIndex = 2;
+            this.btn_LedOff.Text = "LED OFF";
+            this.btn_LedOff.UseVisualStyleBackColor = true;
+            this.btn_LedOff.Click += new System.EventHandler(this.btn_LedOff_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 400);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_LedOff;
+        private System.Windows.Forms.Button btn_LedOn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ HMI_ESP32V1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace HMI_ESP32V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_Open.Enabled = true;
+            btn_Close.Enabled = false;
+            pictureBox1.BackColor = Color.Red;
+            comboBox2.Text = "9600";
+            progressBar1.Value = 0;
+
+            string[] portList = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(portList);
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                serialPort1.Open();
+
+                btn_Open.Enabled = false;
+                btn_Close.Enabled = true;
+                progressBar1.Value = 100;
+                comboBox1.Enabled = false;
+                comboBox2.Enabled = false;
+
+                pictureBox1.BackColor = Color.Red;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                
+            }
+           
+
+        }
+
+        private void btn_LedOff_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    pictureBox1.BackColor = Color.Red;
+                    serialPort1.WriteLine("#Off");
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.WriteLine("#Off");
+                    btn_Open.Enabled = true;
+                    btn_Close.Enabled = false;
+                    progressBar1.Value = 0;
+                    comboBox1.Enabled = true;
+                    comboBox2.Enabled = true;
+
+                    pictureBox1.BackColor = Color.Red;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+
+                }
+            }
+        }
+
+        private void btn_LedOn_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    pictureBox1.BackColor = Color.Lime;
+                    serialPort1.WriteLine("#On");
+                }catch(Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    pictureBox1.BackColor=Color.Red;
+                    serialPort1.WriteLine("#Off");
+                    serialPort1.Close();
+                }catch(Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+    }
+}
